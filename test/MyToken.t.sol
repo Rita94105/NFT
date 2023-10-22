ee148ce2dbf19d957d6fb76d676c5696e75ab9a3
Rita94105 <jenny3500942@gmail.com>
2023-10-22 21:03:55 +0800

    assertEq solve but setUp() fail

--- test/MyToken.t.sol
+++ test/MyToken.t.sol
@@ -8,22 +8,23 @@ contract MyTokenTest is Test{
     MyToken public token;
     address user = makeAddr("user");
 
-    function setup() public {
+    function setUp() public {
         token = new MyToken(user);
     }
 
     function testMint() public{
         vm.startPrank(user);
         token.flipSaleActive();
-        assertEq(token._isSaleActive,true,"open sale fail");
+        bool result = token._isSaleActive();
+        assertEq(result,true,"open sale fail");
         //token.fulfillRandomWords();
         //assertEq(token.s_requestId>0,true,"not random");
         token.safeMint(user);
         string memory closed = token.tokenURI(0);
-        //console2.log(closed);
+        console2.log(closed);
         token.flipReveal();
         string memory opened = token.tokenURI(0);
-        //console2.log(opened);
+        console2.log(opened);
         assertEq(opened,closed,"open success");
     }
 }
